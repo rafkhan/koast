fab8b5ca56ef59ffaf3763399ddd8f9bfadfed6c
Rafael Khan <raf.khan.f@gmail.com>
2014-09-02 22:47:01 -0400

    Status codes!

--- webmachine/koast_wm.js
+++ webmachine/koast_wm.js
@@ -16,14 +16,31 @@ koast.methods = {
 };
 
 var decisions = {
-  knownMethod: function(machine, req, res) {
+  unknownMethod: function(machine, req) {
     var knownMethods = machine.knownMethods || _.values(koast.methods);
     if(_.contains(knownMethods, req.method)) {
+      return false;
+    }
+    return true;
+  },
+
+  uriTooLong: function(machine, req) {
+    if(req.url.length > 4096) {
       return true;
     }
     return false;
+  },
+
+  methodAllowed: function(machine, req) {
+    var allowedMethods = machine.allowedMethods || [koast.methods.GET];
+    if(_.contains(allowedMethods, req.method)) {
+      return true;
+    }
+
+    return false;
   }
 };
+
 
 function runWebMachine(machine) {
   return function() {
@@ -32,14 +49,47 @@ function runWebMachine(machine) {
     var req = this.req,
         res = this.res;
 
-    // If webmachine runs successfully
-    if(decisions.knownMethod(machine, req, res)) {
+    // Known Method?
+    if(decisions.unknownMethod(machine, req)) {
+      res.writeHead(501); // 501 - Not Implemented
+      res.end();
+      return;
+    }
+
+    // URI too long?
+    if(decisions.uriTooLong(machine, req)) {
+      res.writeHead(414); // 414 - Request URI too long
+      res.end();
+      return;
+    }
+
+    // Method Allowed?
+    if(!decisions.methodAllowed(machine, req)) {
+      res.writeHead(405); // 405 - Method Not Allowed
+      res.end();
+      return;
+    }
+
+    if(req.method === koast.methods.GET) {
       machine.handleOk(req, res);
     }
   };
 }
 
 
+//
+// Resource parameters
+//
+// - path:
+//     String
+//     Path to dispatch to from flatiron router.
+//     i.e: "/users/:id/attr"
+//
+// - allowedMethods:
+//     Array
+//     List of HTTP methods allowed on a resource.
+//     i.e: [koast.methods.GET, 'POST', ...]
+//     
 koast.resource = function(machine) {
   // Add current webmachine to available routes
   routeMap[machine.path] = {
@@ -59,15 +109,20 @@ koast.createServer = function() {
 
   return http.createServer(function (req, res) {
     router.dispatch(req, res, function(err) {
-      console.log(err);
+      res.writeHead(err.status || 500);
+      res.write(JSON.stringify(err));
+      res.end();
     });
   });
 };
 
 //***************************************************************************
+//
+//
+//
 koast.resource({
   path: '/asd/:x',
-  knownMethods: [koast.methods.GET],
+  allowedMethods: [koast.methods.GET],
   handleOk: function(req, res) {
     res.write('asd');
     res.end();
