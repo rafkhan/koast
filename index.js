b0171513c1874d68c3ce8ee1d58273a38eec6de9
Rafael Khan <raf.khan.f@gmail.com>
2014-09-24 19:38:06 -0400

    Add bootstrap module

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+'use strict';
+
 var config = require('./lib/config/config-loader');
+var bootstrap = require('./lib/app/bootstrap');
 
 var koast = {};
+
+koast.serve = function () {
+  var app = ();
+  app.listen(3000);
+};
 
 module.exports = exports = koast;
